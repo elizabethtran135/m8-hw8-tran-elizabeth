19448a4734953155d1db32da3e6b4a31235bf21f
Elizabeth Tran <elizabethtran135@gmail.com>
2019-10-19 21:54:14 -0400

    Create hover submenu

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // It is always helpful to use comments in your code!
+var myMainNavLinks = document.getElementsByClassName('main-menu-labels');
+
+function showSubMenu() {
+  console.log(this.nextElementSibling)
+  this.nextElementSibling.classList.remove('hide')
+}
+function hideSubMenu() {
+  console.log(this.nextElementSibling)
+  this.nextElementSibling.classList.add('hide')
+}
+for (i=0; i < myMainNavLinks.length; i++) {
+  myMainNavLinks[i].addEventListener('mouseenter',showSubMenu);
+  myMainNavLinks[i].addEventListener('mouseleave',hideSubMenu);
+}
